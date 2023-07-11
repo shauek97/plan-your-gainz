41ec9e65d683e774fc4362e51d40aa68bb40b768
shauek97 <shauek97@gmail.com>
2023-07-11 22:16:01 +0200

    Added logo

--- src/components/sections/Header/Header.js
+++ src/components/sections/Header/Header.js
@@ -1,10 +1,10 @@
 import styles from "./Header.module.scss";
 
+
 export default function Button() {
   return (
     <header>
-      <div className={styles.logo}></div>
-      <h1 className={styles.title}>STWÓRZ SWÓJ PLAN TRENINGOWY!</h1>
+      <div className={styles.logo}><img src='https://i.postimg.cc/bvvgFbWT/LOGO.png' border='0' alt='LOGO'/></div>
     </header>
   );
 }
